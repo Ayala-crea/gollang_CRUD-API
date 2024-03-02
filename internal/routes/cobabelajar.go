0c59a2d0844b28f64a079e7f1a7818bcf58b8ac1
Ayala-crea <ayalarifki@gmail.com>
2024-03-02 10:14:46 +0700

    delete dan update

--- internal/routes/cobabelajar.go
+++ internal/routes/cobabelajar.go
@@ -10,4 +10,7 @@ func SetupBelajarRoutes(router fiber.Router) {
 	user := router.Group("/belajar")
 	user.Get("/", belajarHandler.GetUsers)
 	user.Post("/tambahdata", belajarHandler.CreateUser)
+	user.Get("/cari/:id_user", belajarHandler.GetUser)
+	user.Delete("/hapus/:id_user", belajarHandler.DeleteUser)
+	user.Put("/update/:id_user", belajarHandler.UpdateData)
 }
--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-	// "fmt"
-
 	"github.com/Ayala-Crea/toko-bersama/database"
 	"github.com/Ayala-Crea/toko-bersama/router"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
-	// Membuat koneksi ke database
-	// database.ConnectDB()
-
 	app := fiber.New()
 
+	// Tambahkan middleware CORS ke aplikasi Fiber
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET, POST, PUT, DELETE",
+		AllowHeaders: "Content-Type",
+	}))
+
+	// Membuat koneksi ke database
 	database.ConnectDB()
 
+	// Menyiapkan rute
 	router.SetupRoutes(app)
 
+	// Mendengarkan permintaan pada port 3000
 	app.Listen(":3000")
-
 }
